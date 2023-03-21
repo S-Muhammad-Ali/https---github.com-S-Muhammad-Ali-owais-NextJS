1f7f15c226d504e6a08da2319d0e7d25ba1d3334
syed Muhammad Ali <syedmuhammad456@gmail.com>
2023-03-21 23:00:46 +0500

    Test
    owais sir instruction

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Let's get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
+        <div>Hello World</div>
+        <p style={{color:"red"}}>I am a new Paragraph</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
